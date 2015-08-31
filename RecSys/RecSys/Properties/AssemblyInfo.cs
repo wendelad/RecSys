906918cad3a5dc6c3330c01c671106eb8fa8beee
Bruno Cabral <brataodream@gmail.com>
2015-08-31 14:05:38 -0300

    WIP: Refatorando para outras classes

--- RecSys/RecSys/Properties/AssemblyInfo.cs
+++ RecSys/RecSys/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyEnsembleLite")]
+[assembly: AssemblyTitle("RecSys")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyEnsembleLite")]
+[assembly: AssemblyProduct("RecSys")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
